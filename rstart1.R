4b149505ec3373ef53695c1468efb0abc07e4fce
jaimandal10 <jaimandal27@gmail.com>
2018-10-25 21:32:13 +0530

    r1

---
+++ rstart1.R
@@ -0,0 +1,36 @@
+x1=3
+x2<-3
+x1
+x2
+(x3=3)
+data()
+?women
+?mtcars
+mtcars
+mtcars$mpg
+attach(mtcars)
+mpg
+library()
+ls()
+rm(list=ls())
+rm(list=ls(all=TRUE))
+x1
+data(mtcars)
+rm(list=ls())
+?mean
+x<-c(0:10,50)
+x
+print(x)
+xm<-mean(x)
+xm
+c(xm, mean(x, trim =.1))
+1:100
+seq(0,100, by=2)
+seq(0,200, by!=2)
+seq(1,200, by=2)
+seq(1,10, length.out=5)
+rep(1,5)
+c("gigi", "hadid")
+rep(c(1,4), times=4)
+rep(c(1,4), each=4)
+rep(c(1,4), times=c(2,6))
